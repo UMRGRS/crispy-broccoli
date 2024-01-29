b7b5ba8b874d120bee339c5e39f819a687bd0495
BorjonZapata <alejandro.borjon@protonmail.com>
2024-01-29 10:17:02 -0600

    Get endponit, para coonsultar las reviews de un producto.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const fs = require('fs')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/consult-reviews/:id', function (req, res){
+    data = fs.readFileSync('json_data/data.json')
+    const jsonData = JSON.parse(data)
+    const { id  } = req.params
+    res.send(jsonData['productos'][id-1]['Info_producto']['reviews'])
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
