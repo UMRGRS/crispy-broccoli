f3288477c5655224fc8469ba58c0f17c1c5e3c67
AlanGtz59 <a1120150064@utch.edu.mx>
2024-01-29 10:58:46 -0600

    Agregar funcion delete

--- server.js
+++ server.js
@@ -3,54 +3,22 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
 const fs = require('fs')
-const path = require('path');
 
-app.use(bodyParser.json())
-
-//Return the API documentation
-app.get('/API-info',(req,res)=> res.sendFile(path.join(__dirname, 'html/index.html')))
-
-/*Receives a json file in the body of the request with this format to create a new product: 
-{
-    "SKU": "",
-    "modelo": "",
-    "departamento": "",
-    "descripcion": "",
-    "pesos": "",
-    "dolares": "" 
-}
-*/
-app.put('/create-product',function (req, res){
-    //Opening and parsing data.json to save the new product
+app.delete('/eliminar/:index', function(req,res) {
     var data_file = fs.readFileSync('json_data/data.json')
     data_object = JSON.parse(data_file)
 
-    //Opening and parsing the template for a new product
-    var temp = fs.readFileSync('json_templates/product_temp.json')
-    var new_product = JSON.parse(temp)
+    data_object["productos"].splice(req.params.index-1, 1);
+     //Converting the data object into json
+     data_to_save = JSON.stringify(data_object,null,4)
 
-    //Assigning the values from the req body to the new product
-    new_product['ID_producto'] = data_object['productos'].length + 1
-    new_product['Info_producto']['SKU'] = req.body['SKU']
-    new_product['Info_producto']['modelo'] = req.body['modelo']
-    new_product['Info_producto']['departamento'] = req.body['departamento']
-    new_product['Info_producto']['descripcion'] = req.body['descripcion']
-    new_product['Info_producto']['precio']['MX'] = req.body['MX']
-    new_product['Info_producto']['precio']['US'] = req.body['US']
-
-    //Adding the new product to the list of products
-    data_object['productos'].push(new_product)
-
-    //Converting the data object into json
-    data_to_save = JSON.stringify(data_object,null,4)
-
-    //Writing the new data into a json file
-    fs.writeFile('json_data/data.json', data_to_save, 'utf8',function(err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-    res.send('Producto agregado con éxito')
+     //Writing the new data into a json file
+     fs.writeFile('json_data/data.json', data_to_save, 'utf8',function(err) {
+         if (err) {
+             console.log(err);
+         }
+     });
+    res.send("Se elimino correctamente")
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
